6485541fe7e22c9501b39e1bee680af1b9668dc4
Luis Eineder <luis.eineder@gmail.com>
2025-08-28 19:09:04 -0700

    Update Kontakt-E-Mail zu info@saifin.de

--- web/src/views/Contact.vue
+++ web/src/views/Contact.vue
@@ -2,6 +2,6 @@
   <section class="p-4 md:p-6">
     <h1 class="text-2xl md:text-3xl mb-3">Kontakt</h1>
     <p class="text-700 mb-2">Kontaktieren Sie uns unter:</p>
-    <p class="text-900 font-bold">Max.Mustermann@gmail.com</p>
+    <p class="text-900 font-bold">info@saifin.de</p>
   </section>
 </template>
